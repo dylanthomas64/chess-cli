e84eae9e9d72882f3e819c1fc3eeddf0b68dfcfe
dylanthomas64 <dylan010299@gmail.com>
2025-01-20 17:31:38 +0000

    refactoring

--- src/board.rs
+++ src/board.rs
@@ -90,7 +90,7 @@ struct CastlingRights {
 impl FromStr for CastlingRights {
     type Err = ParseError;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let len = s.chars.count();
+        let len = s.chars().count();
         if len > 4 {
             return Err(ParseError::ParseCastlingRightsError)
         }
@@ -117,17 +117,93 @@ enum GameState {
     Resignation, 
 }
 
-// struct to hold index values of board.squares
-// created so that the from str can be implemented ie. a1 -> u8
-struct SquaresIndex(u8);
+
+//human readable chess coordinate, than can be converted to an index to squares vector
+struct Coordinate {
+    file: char,
+    rank: u8,
+}
+impl Display for Coordinate {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}{}", self.file, self.rank)
+    }
+}
+impl FromStr for Coordinate {
+    type Err = ParseError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut chars = s.chars();
+        let file: char;
+        let rank: u8;
+        let possible_files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+        if &chars.clone().count() == &2usize {
+            file = chars.next().unwrap();
+            rank = match chars.next().unwrap().to_string().to_string().parse::<u8>() {
+                Ok(rank) => rank,
+                Err(_) => return Err(ParseError::ParseIntError)
+            };
+            for c in possible_files {
+                if c == file {
+                    if (1..=8).contains(&rank) {
+                        return Ok(Coordinate{file, rank})
+                    } else {
+                        return Err(ParseError::ParseCoordinateError("file not in range 1..=8".to_string()))
+                    }
+                }
+            }
+            return Err(ParseError::ParseCoordinateError("file not in range a-h".to_string()))
+            
+        }
+        Err(ParseError::ParseCoordinateError("requires exactly 2 chars eg. a1".to_string()))
+    }
+}
+
+impl TryInto<u8> for Coordinate {
+    type Error = ParseError;
+    fn try_into(self) -> Result<u8, Self::Error> {
+        let file: u8 = match self.file {
+            'a' => 0,
+            'b' => 1,
+            'c' => 2,
+            'd' => 3,
+            'e' => 4,
+            'f' => 5,
+            'g' => 6,
+            'h' => 7,
+            _ => panic!()
+        };
+        let rank: u8 = self.rank - 1;
+        return Ok((8*file) + rank)
+    }
+}
+
+impl TryFrom<u8> for Coordinate {
+    type Error = ParseError;
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        
+        let file = match value % 8 {
+            0 => 'a',
+            1 => 'b',
+            2 => 'c',
+            3 => 'd',
+            4 => 'e',
+            5 => 'f',
+            6 => 'g',
+            7 => 'h',
+            _ => panic!()
+        };
+        let rank = (value / 8) + 1;
+        return Ok(Coordinate{file, rank})
+        
+    }
+}
 
 // contains board representation and relevant information
-struct Board {
+pub struct Board {
     // where a1 == 0, a2 == 1, h8 == 63
     squares: Vec<Option<Piece>>,
     active_colour: Colour,
     castling_rights: CastlingRights,
-    en_passant_target_square: Option<SquaresIndex>,
+    en_passant_target_square: Option<Coordinate>,
     // required for 50 move rule
     half_move_clock: u8,
     // starts at 1 and increments after black's move
@@ -135,21 +211,84 @@ struct Board {
 }
 
 impl Board {
-    fn new(fen: String) -> Result<Board, ParseError> {
+    pub fn new(fen: String) -> Result<Board, ParseError> {
         Board::from_str(&fen)
     }
-    fn default() -> Board {
+    pub fn default() -> Board {
         Board::new("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1".to_string()).unwrap()
+    }
+}
+impl Display for Board {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+
+        let v: Vec<String> = self.squares.iter().map(|opt| match opt {
+            Some(piece) => format!("{}", piece),
+            None => " ".to_string(),
+        })
+        .collect();
+
+        let row1 = format!(
+            "1 |  {}  |  {}  |  {}  |  {}  |  {}  |  {}  |  {}  |  {}  |",
+            v[0], v[1], v[2], v[3], v[4], v[5], v[6], v[7]
+        );
+        let row2 = format!(
+            "2 |  {}  |  {}  |  {}  |  {}  |  {}  |  {}  |  {}  |  {}  |",
+            v[8], v[9], v[10], v[11], v[12], v[13], v[14], v[15]
+        );
+        let row3 = format!(
+            "3 |  {}  |  {}  |  {}  |  {}  |  {}  |  {}  |  {}  |  {}  |",
+            v[16], v[17], v[18], v[19], v[20], v[21], v[22], v[23]
+        );
+        let row4 = format!(
+            "4 |  {}  |  {}  |  {}  |  {}  |  {}  |  {}  |  {}  |  {}  |",
+            v[24], v[25], v[26], v[27], v[28], v[29], v[30], v[31]
+        );
+        let row5 = format!(
+            "5 |  {}  |  {}  |  {}  |  {}  |  {}  |  {}  |  {}  |  {}  |",
+            v[32], v[33], v[34], v[35], v[36], v[37], v[38], v[39]
+        );
+        let row6 = format!(
+            "6 |  {}  |  {}  |  {}  |  {}  |  {}  |  {}  |  {}  |  {}  |",
+            v[40], v[41], v[42], v[43], v[44], v[45], v[46], v[47]
+        );
+        let row7 = format!(
+            "7 |  {}  |  {}  |  {}  |  {}  |  {}  |  {}  |  {}  |  {}  |",
+            v[48], v[49], v[50], v[51], v[52], v[53], v[54], v[55]
+        );
+        let row8 = format!(
+            "8 |  {}  |  {}  |  {}  |  {}  |  {}  |  {}  |  {}  |  {}  |",
+            v[56], v[57], v[58], v[59], v[60], v[61], v[62], v[63]
+        );
+        
+        write!(f, "  —------------------------------------------------\n")?;
+        write!(f, "{}\n", row8)?;
+        write!(f, "  —------------------------------------------------\n")?;
+        write!(f, "{}\n", row7)?;
+        write!(f, "  —------------------------------------------------\n")?;
+        write!(f, "{}\n", row6)?;
+        write!(f, "  —------------------------------------------------\n")?;
+        write!(f, "{}\n", row5)?;
+        write!(f, "  —------------------------------------------------\n")?;
+        write!(f, "{}\n", row4)?;
+        write!(f, "  —------------------------------------------------\n")?;
+        write!(f, "{}\n", row3)?;
+        write!(f, "  —------------------------------------------------\n")?;
+        write!(f, "{}\n", row2)?;
+        write!(f, "  —------------------------------------------------\n")?;
+        write!(f, "{}\n", row1)?;
+        write!(f, "  —------------------------------------------------\n")?;
+        write!(f, "     A     B     C     D     E     F     G     H   \n")
     }
 }
 // error types
 #[derive(Debug)]
-enum ParseError {
+pub enum ParseError {
     ParseFenError(String),
     ParseCharError,
     ParseIntError,
     ParseColourError,
     ParseCastlingRightsError,
+    ParseCoordinateError(String),
 }
 
 impl FromStr for Board {
@@ -158,7 +297,7 @@ impl FromStr for Board {
         // accept only valid fen strings and return a board
         // startpos = rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1
         // possible pos = rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2
-        let fen_it = s.split_ascii_whitespace();
+        let mut fen_it = s.split_ascii_whitespace();
         let fen_piece_data = match fen_it.next() {
             Some(str) => str,
             None => return Err(ParseError::ParseFenError("no piece data found".to_string()))
@@ -173,16 +312,21 @@ impl FromStr for Board {
             None => return Err(ParseError::ParseFenError("no castling rights data found".to_string()))
         };
         let en_passant_target_square = match fen_it.next() {
-            todo!()
-            Some(str) => st
+            Some(str) => { if str == '-'.to_string() {
+                None
+            } else {
+                Some(str.parse::<Coordinate>().unwrap())
+            }
+                
+            },
             None => return Err(ParseError::ParseFenError("no en passant target data found".to_string()))
         };
         let half_move_clock = match fen_it.next() {
-            Some(str) => str.parse::<u8>()?,
+            Some(str) => str.parse::<u8>().unwrap(),
             None => return Err(ParseError::ParseFenError("no half move clock data found".to_string()))
         };
         let full_move_number = match fen_it.next() {
-            Some(str) => str.parse::<usize>()?,
+            Some(str) => str.parse::<usize>().unwrap(),
             None => return Err(ParseError::ParseFenError("no full move number data found".to_string()))
         };
 
--- src/main.rs
+++ src/main.rs
@@ -1,97 +1,10 @@
-use chess::{Board, Player};
-use clap::Parser;
+
 use std::io;
 use std::io::Write;
-
-mod chess;
-//mod board;
+mod board;
+use crate::board::Board;
 
 fn main() {
-    //println!("♔ 	♕ 	♖ 	♗ 	♘ 	♙ 	♚ 	♛ 	♜ 	♝ 	♞ 	♟ ");
-    let mut board = Board::new();
-    let white = Player::White;
-    let black = Player::Black;
-
-    /* loop {
-                loop {get user white input -> if good play move -> if err continue, else break}
-                loop {black}
-
-
-    }
-
-    */
-
-    board.print();
-
-    'main: loop {
-        // main loop
-        loop {
-            // white turn
-            match get_usr_input(&white) {
-                Ok(usr_input) => {
-                    if usr_input == "q" {
-                        break 'main;
-                    };
-                    if let Err(err) = board.play_move(&white, &usr_input) {
-                        eprintln!("{:?}", err);
-                        continue;
-                    } else {
-                        break;
-                    }
-                }
-                Err(err) => {
-                    eprintln!("{:?}", err);
-                    continue;
-                }
-            }
-        }
-        loop {
-            // black turn
-            match get_usr_input(&black) {
-                Ok(usr_input) => {
-                    if usr_input == "q" {
-                        break 'main;
-                    };
-                    if let Err(err) = board.play_move(&black, &usr_input) {
-                        eprintln!("{:?}", err);
-                        continue;
-                    } else {
-                        break;
-                    }
-                }
-                Err(err) => {
-                    eprintln!("{:?}", err);
-                    continue;
-                }
-            }
-        }
-    }
-
-    println!("Exiting program...");
+    let board = Board::default();
+    println!("{}", board);
 }
-
-fn get_usr_input(player: &Player) -> Result<String, std::io::Error> {
-    let mut usr_input = String::new();
-    match player {
-        Player::White => {
-            println!("White to move: ");
-        }
-        Player::Black => {
-            println!("Black to move: ");
-        }
-    }
-
-    usr_input.clear();
-    io::stdout().flush().expect("Cannot flush stdout");
-    io::stdin().read_line(&mut usr_input)?;
-
-    usr_input = usr_input
-        .strip_suffix("\r\n")
-        .or(usr_input.strip_suffix("\n"))
-        .unwrap_or(&usr_input)
-        .to_string();
-    Ok(usr_input)
-}
-
-#[derive(Parser, Debug)]
-struct Args {}
